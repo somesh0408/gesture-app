40fb233c249e3e0597e101ba5c1cbd104db89e4a
sonai517 <sonalialhate517@gmail.com>
2025-04-11 15:05:34 +0530

    Fix: bind Flask app to Render PORT

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pygame
 import os
 import tempfile
 from PIL import ImageFont, ImageDraw, Image
+
 app = Flask(__name__)
 
 # Load model
@@ -143,6 +144,4 @@ def set_language():
     return ("", 204)
 
 if __name__ == '__main__':
-    import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
